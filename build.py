40cf099d2fc2e8e92945e5cb07153955649131c4
Alexis Lopez Zubieta <contact@azubieta.net>
2019-05-04 12:18:10 -0500

    Only use bincrafters remote

--- build.py
+++ build.py
@@ -1,10 +1,7 @@
 from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    remotes = [
-        ("https://api.bintray.com/conan/appimage-conan-community/public-conan", True, "appimage-conan-community"),
-        ("https://api.bintray.com/conan/azubieta/AppImage", True, "azubieta")
-    ]
+    remotes = [("https://api.bintray.com/conan/bincrafters/public-conan", True, "bincrafters")]
 
     builder = ConanMultiPackager(remotes=remotes, build_policy="missing", build_types=["Release"])
     builder.add_common_builds(shared_option_name="kf5-attica:shared")
